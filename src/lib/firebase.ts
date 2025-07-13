9017129b78b6ba3532fce7baff951a5750978b5a
ESTUDIANTE MRHS Maria Camila Madrigal Ávila <maria.c.madrigal@colegiomonterrosales.edu.co>
2025-07-13 03:09:11 +0000

    jeje, como cosa rara sirve sin funcionar! será que elimino la base de da

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { initializeApp, getApp, getApps, type FirebaseApp } from "firebase/app";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration will be updated here
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCArr2NtU7FN5LTjVRq7cnmrhIVGS_8nOs",
   authDomain: "agrinavigate.firebaseapp.com",
   projectId: "agrinavigate",
   storageBucket: "agrinavigate.firebasestorage.app",
   messagingSenderId: "619147792018",
-  appId: "1:619147792018:web:0d779d71bfba162962a04b"
+  appId: "1:619147792018:web:b7591b27fd28c34a62a04b"
 };
 
 // Initialize Firebase
