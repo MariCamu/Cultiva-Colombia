df87a587d9c9f24b019a8a67dd21bd4fdf72999f
ESTUDIANTE MRHS Maria Camila Madrigal Ávila <maria.c.madrigal@colegiomonterrosales.edu.co>
2025-07-25 20:01:25 +0000

    por favor pon en privado todas las credenciales. Lo subi sin querer a gi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCArr2NtU7FN5LTjVRq7cnmrhIVGS_8nOs",
-  authDomain: "agrinavigate.firebaseapp.com",
-  projectId: "agrinavigate",
-  storageBucket: "agrinavigate.firebasestorage.app",
-  messagingSenderId: "619147792018",
-  appId: "1:619147792018:web:b7591b27fd28c34a62a04b"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
