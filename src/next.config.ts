5f52eb63b4ede19c64183e69d80add88f2897339
ESTUDIANTE MRHS Maria Camila Madrigal Ávila <maria.c.madrigal@colegiomonterrosales.edu.co>
2025-08-05 04:16:22 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,32 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
